9375ee6145ad789097af736f3cbef6b26249e7b7
Christian Miguel Andal <165505365+ChristianAndal@users.noreply.github.com>
2025-09-17 22:03:48 +0800

    var

---
+++ print_Statement.php
@@ -0,0 +1,19 @@
+<?php
+// single line comment 
+
+/* multi line 
+comment */
+
+// print statement 
+echo "Hello world ";
+
+// variable
+// integer
+$number1 = 2;
+$number2 = 5;
+echo ($number1 + $number2);
+//string
+$str1 = "hatdog";
+$str2 = " yum";
+echo $str1 . $str2;
+?>
